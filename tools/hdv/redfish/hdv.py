8cee1fded4dd11801ea08d1bbfaf914b01e62430
chenliangyjy <chenliangyjy@chinamobile.com>
2020-03-19 10:10:00 +0800

    fix flake8 checking issue of hdv
    Signed-off-by: chenliangyjy <chenliangyjy@chinamobile.com>
    Change-Id: Ie779c186635e57fe5d900df7326e4a704ebcad42

--- tools/hdv/redfish/hdv.py
+++ tools/hdv/redfish/hdv.py
@@ -13,8 +13,8 @@ hdv tools
  additional config for supporting two modes
  - excel: tools will parse the depend_id sheet and cases sheet and \
  execute test case and write report back to excel
- - yaml: tools will parse depends.yaml and cases.yaml and execute test case and\
-  write a report.yaml
+ - yaml: tools will parse depends.yaml and cases.yaml and execute test case ¡¢
+ and write a report.yaml
  theory:
    either test case can be finished by one restful request,
    or an additional request needed to get dependency parent resource.
@@ -23,24 +23,25 @@ hdv tools
 import argparse
 from hdv_redfish import run as run_case
 
+
 def parse_args():
     '''
     parse arguments
     '''
     parser = argparse.ArgumentParser(description="hdv tool by redfish, \
     check readme under ./docs")
-    parser.add_argument('--version', action='version', \
+    parser.add_argument('--version', action='version',
                         version='%(prog)s 0.1', help="show tool version")
-    parser.add_argument('--config', type=str, default="./conf/config.yaml", \
+    parser.add_argument('--config', type=str, default="./conf/config.yaml",
                         help="given global config.yaml file")
-    parser.add_argument('--file_type', type=str, default="excel", \
+    parser.add_argument('--file_type', type=str, default="excel",
                         help="config file type, [yaml|excel],default is excel")
-    parser.add_argument('--case_yaml', type=str, default="./conf/cases.yaml", \
+    parser.add_argument('--case_yaml', type=str, default="./conf/cases.yaml",
                         help="case yaml file, uesd if file_type = yaml")
-    parser.add_argument('--depends_yaml', type=str, \
-                        default="./conf/depends.yaml",\
-                         help="depends yaml file,uesd if file_type = yaml")
-    parser.add_argument('--case_excel', type=str, default="./conf/cases.xlsx", \
+    parser.add_argument('--depends_yaml', type=str,
+                        default="./conf/depends.yaml",
+                        help="depends yaml file,uesd if file_type = yaml")
+    parser.add_argument('--case_excel', type=str, default="./conf/cases.xlsx",
                         help="excel case file used if file_type = excel")
     args = parser.parse_args()
     return args
@@ -51,8 +52,8 @@ def main():
     main function
     '''
     args = parse_args()
-    run_case(args.config, args.case_excel, args.depends_yaml, args.case_yaml,\
-              args.file_type)
+    run_case(args.config, args.case_excel, args.depends_yaml, args.case_yaml,
+             args.file_type)
 
 
 if __name__ == "__main__":
